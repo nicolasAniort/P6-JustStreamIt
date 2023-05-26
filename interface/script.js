defa34ced79ae307fadc20d4c97b34dbee68e662
nicolasAniort <nicolas.aniort@gmail.com>
2023-05-26 23:11:10 +0200

    slider arrow

--- interface/script.js
+++ interface/script.js
@@ -36,7 +36,7 @@ let categories =[
     "Crime", 
     "Mystery"
 ] 
-//fonction de verification de fonctionnement du serveur/de l'url
+//fonction de verification de fonctionnement du serveur de l'url
 fetch(url_base)
     .then(res => res.json())
     .catch(error => console.error('il y a eu une erreur...'));
@@ -65,6 +65,9 @@ function fetchMovies() {
                 const url_resume = data.results[cpt].url;
                 const imag = data.results[cpt].image_url;
                 const movie = document.createElement('img');
+                 // Ajout des classes "slide" et "movie" à chaque élément
+                detailmovie.classList.add('slide');
+                movie.classList.add('movie');
                 slider.appendChild(detailmovie);
                 detailmovie.appendChild(movie);
                 movie.classList.add('movie');
@@ -113,6 +116,8 @@ slider.classList.add('slider');
 slidercontainer.appendChild(slider);  
 fetchMovies();
 
+// Get the modal
+var modal = document.getElementById("modal_movie");
 //Creation des 3 blocs et leur contenus
 function fetchCategories() {
 
@@ -143,72 +148,110 @@ function fetchCategories() {
           for(movies_cpt=0;movies_cpt<7;movies_cpt++){
                     let detailmovie = document.createElement('a');
                     let movie_pic = document.createElement('img');
+                    // movie_pic.setAttribute('id', data.results[movie_cpt].id);
+                    movie_pic.classList.add("movie", movies_cpt+"movie_pic");
                     let bouton = document.createElement('button');
                     slider.appendChild(detailmovie);
-                    movie_pic.classList.add("movie", movies_cpt+"movie_pic");
+                    var id = data.results[movies_cpt].id;
                     detailmovie.appendChild(movie_pic);
-                    bouton.classList.add("modal-btn","modal-trigger");
+                    // bouton.appendChild(movie_pic);
+                    bouton.id = id
+                    bouton.classList.add("modal-trigger");
+                    bouton.type ="button";
                     bouton.textContent = 'PLUS';
+                    bouton.addEventListener("click", function(id) {
+                      modal.style.display = "block";
+                      ModalWindows(id);
+                    });
                     detailmovie.appendChild(bouton);
-                    //detailmovie.addEventListener('click',ModalWindows(data.results[movies_cpt].url));
-                    //detailmovie.setAttribute('onclick', "ModalWindows('" + data.results[movies_cpt].url + "')");
+                    // movie_pic.id = data.results[movie_cpt].id;
                     movie_pic.alt = 'affiche du film:' + data.results[movies_cpt].title;
                     movie_pic.src = data.results[movies_cpt].image_url;
-                    movie_pic.onclick = ModalWindows(data.results[movies_cpt].url);
+                    // movie_pic.onclick = ModalWindows(data.results[movies_cpt].url);
                   }
                 })
                   i=i+1;
                 })
                 
 }
-     
 fetchCategories()
-//Propriété fenetre modale
-const modalContainer = document.querySelector(".modal-container");
-const modalTriggers = document.querySelectorAll(".modal-trigger");
-
-modalTriggers.forEach(trigger => trigger.addEventListener("click", toggleModal));
-
-function toggleModal () {
-    modalContainer.classList.toggle("active");
+// SLIDER:Fonction de défilement latéral
+function scrollSlider(box, direction) {
+  const slider = box.querySelector('.slider');
+  const scrollAmount = direction === 'left' ? -slider.offsetWidth : slider.offsetWidth;
+  slider.scrollBy({
+    top: 0,
+    left: scrollAmount,
+    behavior: 'smooth'
+  });
 }
+// SLIDER :Ajout des flèches de défilement aux slidebox
+const slideBoxes = document.querySelectorAll('.slider-container');
+slideBoxes.forEach(box => {
+  const arrowLeft = document.createElement('div');
+  arrowLeft.classList.add('slider-arrow', 'arrow-left');
+  arrowLeft.style.backgroundImage = "url('./img/a-left.png')"; 
+  arrowLeft.addEventListener('click', () => scrollSlider(box, 'left'));
+  box.appendChild(arrowLeft);
 
+  const arrowRight = document.createElement('div');
+  arrowRight.classList.add('slider-arrow', 'arrow-right');
+  arrowRight.style.backgroundImage = "url('./img/a-right.png')";
+  arrowRight.addEventListener('click', () => scrollSlider(box, 'right'));
+  box.appendChild(arrowRight);
+});
+// MODALE:Ajout de l'événement de clic au bouton "Plus"
+const moreButtons = document.querySelectorAll('.modal-btn');
+moreButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const movieURL = button.parentNode.getAttribute('href');
+    ModalWindows(movieURL);
+  });
+});
+//MODALE:Propriété fenetre modale
+const modalContainer = document.querySelector('#modal_movie', '.modal-container');
+const modalTriggers = document.querySelectorAll('.modal-trigger');
 
-//creation de la fenetre modale d'un film 
-function ModalWindows(url){
-    fetch(url)
-        .then(res =>res.json())
-        .then(data => {
-            let _title = document.querySelector('#headerModal__originalTitle');
-            let _filmImage = document.querySelector('#headerModal__originalTitle');
-            let _genres = document.querySelector('#infoModalText__genres');
-            let _publishdate = document.querySelector('#infoModalText__datePublished');
-            let _rated = document.querySelector('#infoModalText__rated');
-            let _imdbScore = document.querySelector('#infoModalText__imdbScore');
-            let _directors = document.querySelector('#infoModalText__directors');
-            let _actors = document.querySelector('#infoModalText__actors');
-            let _duration = document.querySelector('#infoModalText__duration');
-            let _countries = document.querySelector('#infoModalText__countries');
-            //let _avg_vote = document.querySelector('#infoModalText__avg_vote');
-            let _resume = document.querySelector('#infoModalText__longDescription');
-            //console.log('data:',data );
-            //console.log('results:',data.results[0] );
-            //console.log('title:',data.title );
-            _title.textContent = data.title;
-            _filmImage.src = data.image_url;
-            _genres.textContent = data.genres;
-            _publishdate.textContent = data.date_published;
-            _rated.textContent = data.rated;
-            _imdbScore.textContent = data.imdb_score;
-            _directors.textContent = data.directors;
-            _actors.textContent = data.actors;
-            _duration.textContent = data.duration;
-            _countries.textContent = data.countries;
-            /*if ((data.avg_vote)!==null) {
-            _avg_vote.textContent = data.avg_vote;
-            } else {
-            _avg_vote.textContent = 'Pas de classement';
-            }*/
-            _resume.textContent = data.long_description;
-         } )                   
+modalTriggers.forEach(trigger => trigger.addEventListener('click', toggleModal));
+
+function toggleModal (event) {
+//MODALE: Pour afficher la modal
+  modalContainer.classList.add('active');  
+  modalContainer.classList.toggle('active');
+}
+//MODALE:creation de la fenetre modale d'un film 
+function ModalWindows(id){
+  if (typeof(id) === "object") {
+    id = id.target.id;
+  }
+  let url_film = "http://localhost:8000/api/v1/titles/"+id
+  fetch(url_film)
+      .then(res =>res.json())
+      .then(data => {
+          //creation des variables de champs
+          let _title = document.querySelector('#headerModal__originalTitle');
+          let _filmImage = document.querySelector('#headerModal__filmImage');
+          let _genres = document.querySelector('#infoModalText__genres');
+          let _publishdate = document.querySelector('#infoModalText__datePublished');
+          let _rated = document.querySelector('#infoModalText__rated');
+          let _imdbScore = document.querySelector('#infoModalText__imdbScore');
+          let _directors = document.querySelector('#infoModalText__directors');
+          let _actors = document.querySelector('#infoModalText__actors');
+          let _duration = document.querySelector('#infoModalText__duration');
+          let _countries = document.querySelector('#infoModalText__countries');
+          let _resume = document.querySelector('#infoModalText__longDescription');
+          //alimentation des variables de champs
+          _title.textContent = data.title;
+          console.log(data.title);
+          _filmImage.src = data.image_url;
+          _genres.textContent = data.genres;
+          _publishdate.textContent = data.date_published;
+          _rated.textContent = data.rated;
+          _imdbScore.textContent = data.imdb_score;
+          _directors.textContent = data.directors;
+          _actors.textContent = data.actors;
+          _duration.textContent = data.duration;
+          _countries.textContent = data.countries;
+          _resume.textContent = data.long_description;
+        } )                   
 }
