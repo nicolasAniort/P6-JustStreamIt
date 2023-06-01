d0bdb41c7bb8ba3dc0f58aded831502fbe282c20
nicolasAniort <nicolas.aniort@gmail.com>
2023-06-01 18:40:16 +0200

    bouton css et MAJ js pour le bouton
    Signed-off-by: nicolasAniort <nicolas.aniort@gmail.com>

--- interface/script.js
+++ interface/script.js
@@ -1,5 +1,4 @@
 // constante globales
-const id_mov = document.querySelector('#id');
 const title_mov = document.querySelector('#bestFilmName');
 const img_mov = document.querySelector('#bestFilm_img');
 const resume_movie = document.querySelector('#bestFilmDescription'); 
@@ -12,24 +11,6 @@ const url_suffixe_genre = '&sort_by=-imdb_score&page_size=7';
 //Variables globales
 let new_url;
 
-
-//definition d'un objet moviedetails
-class moviedetails {
-    constructor(url){
-    this.image_url = "image_url" ,
-    this.title = "title" ,
-    this.genres = "genres" ,
-    this.date_published = "date-published",
-    this.votes = "votes", 
-    this.imbd_score = "imbd_score" , 
-    this.directors = "directors" ,
-    this.actors = "actors",
-    this.duration = "duration",
-    this.countries = "countries" ,
-    this.avg_vote = "avg_vote",
-    this.description = "description"
-    }
-}
 //liste des categories 
 let categories =[
     "Comedy",
@@ -52,6 +33,27 @@ function resumeBestFilm(url){
             return resumedb;
         })     
 }
+//Fonction de recupération du meilleur Film
+async function fetchBestFilm(url) {
+  const res = await fetch(url);
+  const data = await res.json();
+
+  const title = data.results[0].title;
+  const imag = data.results[0].image_url;
+  const url_resume = data.results[0].url;
+
+  title_mov.textContent = title;
+  img_mov.src = imag;
+
+  try {
+    const resume = await resumeBestFilm(url_resume);
+    resume_movie.textContent = resume;
+  } catch (error) {
+    console.error(error);
+  }
+}
+// Generation de l'interface du Meilleur Film toutes catégories  
+fetchBestFilm(url_bestfilm);
 //fonction de recuperation des meilleurs films
 function fetchMovies() {
     for (let film_count = 1; film_count < 8; film_count++) {
@@ -65,59 +67,46 @@ function fetchMovies() {
                 const url_resume = data.results[cpt].url;
                 const imag = data.results[cpt].image_url;
                 const movie = document.createElement('img');
+                let boutons = document.createElement('button');
                  // Ajout des classes "slide" et "movie" à chaque élément
                 detailmovie.classList.add('slide');
                 movie.classList.add('movie');
                 slider.appendChild(detailmovie);
+                var ident = data.results[film_count].id;
                 detailmovie.appendChild(movie);
+                boutons.id = ident;
+                boutons.classList.add("modal-trigger");
+                boutons.type ="button";
+                boutons.textContent = '+';
+                boutons.addEventListener("click", function(id) {
+                  // Déclarez et initialisez la variable modal
+                  var modal = document.querySelector(".modal-close"); 
+                  modal.className ="modal";
+                  ModalWindows(id);
+                  modalContainer.classList.add('active'); // ligne pour afficher la modale
+                });
+                detailmovie.appendChild(boutons);
+                movie.alt = 'affiche du film:' + data.results[film_count].title;
+                movie.src = data.results[film_count].image_url;
                 movie.classList.add('movie');
-                detailmovie.setAttribute('href', url_resume);
-                movie.alt = 'affiche du film:' + title;
-                movie.src = imag;
             })
     }
-  }
-//Fonction de recupération du meilleur Film
-async function fetchBestFilm(url) {
-    const res = await fetch(url);
-    const data = await res.json();
-  
-    const title = data.results[0].title;
-    const imag = data.results[0].image_url;
-    const url_resume = data.results[0].url;
-  
-    title_mov.textContent = title;
-    img_mov.src = imag;
-  
-    try {
-      const resume = await resumeBestFilm(url_resume);
-      resume_movie.textContent = resume;
-    } catch (error) {
-      console.error(error);
-    }
-  
-     //const bckg_best_film = document.querySelector('.imgBestFilm');
-     //bckg_best_film.style.backgroundImage = `url(${imag})`;
-     //bckg_best_film.style.backgroundSize = 'cover';
-  }
-// Generation de l'interface du Meilleur Film toutes catégories  
-  fetchBestFilm(url_bestfilm);
-  
+  } 
 //Generation de l'interface des meilleurs films toutes categories
 const moviescontain = document.querySelector('#moviescontain')
 let slidercontainer = document.createElement('div');
 let slider = document.createElement('div');
 moviescontain.classList.add('slider-1');
+
 //creation du bloc contenant le bloc listant les films
 slidercontainer.classList.add('slider-container');
 moviescontain.appendChild(slidercontainer);
+
 //creation du bloc contenant les films
 slider.classList.add('slider');
 slidercontainer.appendChild(slider);  
 fetchMovies();
-
-// Get the modal
-var modal = document.getElementById("modal_movie");
+ 
 //Creation des 3 blocs et leur contenus
 function fetchCategories() {
 
@@ -127,7 +116,6 @@ function fetchCategories() {
     let url_temps = url_prefixe_genre + categorie+ url_suffixe_genre
     let boxslide = document.querySelector('#slidebox'+(i));
     let title_bestfilms = document.createElement('h1');
-    //let moviescontain = document.createElement('div');
     let slider = document.createElement('div');
     let slidercontainer = document.createElement('div');   
     //ajouter un titre au bloc
@@ -140,7 +128,7 @@ function fetchCategories() {
     boxslide.appendChild(slidercontainer);
     slider.classList.add('slider');
     slidercontainer.appendChild(slider);
-    //Boucle de recuperation des affiches pour chaque film
+    //Boucle de recuperation des informations pour chaque film
         let url_temp = url_temps;
         fetch(url_temp)
         .then(res =>res.json())
@@ -148,26 +136,25 @@ function fetchCategories() {
           for(movies_cpt=0;movies_cpt<7;movies_cpt++){
                     let detailmovie = document.createElement('a');
                     let movie_pic = document.createElement('img');
-                    // movie_pic.setAttribute('id', data.results[movie_cpt].id);
                     movie_pic.classList.add("movie", movies_cpt+"movie_pic");
                     let bouton = document.createElement('button');
                     slider.appendChild(detailmovie);
                     var id = data.results[movies_cpt].id;
                     detailmovie.appendChild(movie_pic);
-                    // bouton.appendChild(movie_pic);
                     bouton.id = id
                     bouton.classList.add("modal-trigger");
                     bouton.type ="button";
-                    bouton.textContent = 'PLUS';
+                    bouton.textContent = '+';
                     bouton.addEventListener("click", function(id) {
-                      modal.style.display = "block";
+                      // Déclarez et initialisez la variable modal
+                      var modal = document.querySelector(".modal-close"); 
+                      modal.className ="modal";
                       ModalWindows(id);
+                      modalContainer.classList.add('active'); // ligne pour afficher la modale
                     });
                     detailmovie.appendChild(bouton);
-                    // movie_pic.id = data.results[movie_cpt].id;
                     movie_pic.alt = 'affiche du film:' + data.results[movies_cpt].title;
                     movie_pic.src = data.results[movies_cpt].image_url;
-                    // movie_pic.onclick = ModalWindows(data.results[movies_cpt].url);
                   }
                 })
                   i=i+1;
@@ -208,6 +195,15 @@ moreButtons.forEach(button => {
     ModalWindows(movieURL);
   });
 });
+//MODALE: fermeture de la fenetre modale par bouton X
+// Cibler l'élément ou le bouton de fermeture de la fenêtre modale
+const closeModalButton = document.getElementById('modal_close_button');
+// Ajouter un gestionnaire d'événements pour l'événement de clic
+closeModalButton.addEventListener('click', function() {
+  var modal = document.querySelector(".modal");
+  // Modifier la propriété display pour masquer la fenêtre modale
+  modal.className ="modal-close";
+});
 //MODALE:Propriété fenetre modale
 const modalContainer = document.querySelector('#modal_movie', '.modal-container');
 const modalTriggers = document.querySelectorAll('.modal-trigger');
@@ -229,6 +225,7 @@ function ModalWindows(id){
       .then(res =>res.json())
       .then(data => {
           //creation des variables de champs
+          let modaltitle = document.querySelector('#headerModal_Title_originalTitle');
           let _title = document.querySelector('#headerModal__originalTitle');
           let _filmImage = document.querySelector('#headerModal__filmImage');
           let _genres = document.querySelector('#infoModalText__genres');
@@ -241,8 +238,8 @@ function ModalWindows(id){
           let _countries = document.querySelector('#infoModalText__countries');
           let _resume = document.querySelector('#infoModalText__longDescription');
           //alimentation des variables de champs
+          modaltitle.textContent = data.title;
           _title.textContent = data.title;
-          console.log(data.title);
           _filmImage.src = data.image_url;
           _genres.textContent = data.genres;
           _publishdate.textContent = data.date_published;
